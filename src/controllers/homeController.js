e4b4f8c08042c3b366219faa8a1a850713ab78b2
nmargenov <nikolay_margenov@yahoo.com>
2023-06-06 21:14:27 +0300

    Search works

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,9 +3,24 @@ const { getAllCubes } = require('../managers/cubeManager');
 const router = require('express').Router();
 
 router.get(['/','/index'],async(req,res)=>{
-    const cubes = await getAllCubes().lean();
+    const {search,from,to} = req.query;
 
-    res.status(302).render('index',{cubes});
+    
+    let cubes = await getAllCubes().lean();
+    
+    if(search){
+        cubes = cubes.filter(cube=>cube.name.toLowerCase().includes(search.trim().toLowerCase()));
+    }
+
+    if(from){
+        cubes = cubes.filter(cube=>cube.difficultyLevel>=Number(from));
+    }
+
+    if(to){
+        cubes = cubes.filter(cube=>cube.difficultyLevel<=Number(to));
+    }
+
+    res.status(302).render('index',{cubes,search,from,to});
 });
 
 router.get('/about',(req,res)=>{
